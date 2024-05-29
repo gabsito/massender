f594673175b1f832148e7a5fd74178f56e85eb90
gabsito <59795688+gabsito@users.noreply.github.com>
2024-05-29 15:01:49 -0500

    inicializar componentes sidebar, header y routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestViewsComponent } from './test-views/test-views.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'test', component: TestViewsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { LoginViewComponent } from './login-view/login-view.component';
+import { TestViewsComponent } from './test-views/test-views.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    LoginViewComponent,
+    TestViewsComponent,
+    HeaderComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
