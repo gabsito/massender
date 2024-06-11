0f35b0c548bbffd76d6543ed5687f49a6dd9cc08
gabsito <59795688+gabsito@users.noreply.github.com>
2024-06-11 16:34:49 -0500

    toggle menu

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  showMenu: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
   }
 
 }
