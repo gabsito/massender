2566c129a8493a196aaf5ef16a6fd99fbb129276
gabsito <59795688+gabsito@users.noreply.github.com>
2024-06-26 12:43:06 -0500

    commit pre rebase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,18 @@ import { EmpresasComponent } from './dashboard/empresas/empresas.component';
 import { DestinatariosComponent } from './dashboard/destinatarios/destinatarios.component';
 import { PagosComponent } from './dashboard/pagos/pagos.component';
 import { CargadestinatariosComponent } from './dashboard/cargadestinatarios/cargadestinatarios.component';
+import { LoginComponent } from './login/login.component';
+import { DashComponent } from './dash/dash.component';
 
 export const routes: Routes = [
-  { path: 'empresas', component: EmpresasComponent },
-  { path: 'destinatarios', component: DestinatariosComponent },
-  { path: 'pagos', component: PagosComponent },
-  { path: 'destinatarios/:id/importar', component: CargadestinatariosComponent}
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {
+    path: 'dashboard', component: DashComponent, children: [ //all users
+      { path: 'empresas', component: EmpresasComponent }, //superadmin
+      { path: 'destinatarios', component: DestinatariosComponent }, //all users
+      { path: 'pagos', component: PagosComponent },
+      { path: 'destinatarios/:id/importar', component: CargadestinatariosComponent}, //superadmin + admin
+    ]
+  },
+  { path: 'login', component: LoginComponent }, //all users
 ];
