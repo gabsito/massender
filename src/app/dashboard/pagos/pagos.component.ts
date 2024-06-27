d65e168ea79ec53a5acdf4dcffa4cdb39eeb5ea9
alexisloor <caalloor@espol.edu.ec>
2024-06-27 00:37:49 -0500

    validacion

--- src/app/dashboard/pagos/pagos.component.ts
+++ src/app/dashboard/pagos/pagos.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-pagos',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './pagos.component.html',
   styleUrls: ['./pagos.component.css']
 })
